9e04982b6844d5721fdd7cacf74a99a64bb4957b
dapoadedire <adedireadedapo19@gmail.com>
2023-09-20 08:51:29 +0100

    🚀feat(Home) add link to user gallery 🛠️refactor(Home) improve conditional rendering 🛠️refactor(data.js), reduce images to make page load faster

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,8 @@ import { NonSecureCard } from "../components/NonSecureCard";
 import { galleryList } from "../assets/data.js";
 import { Children } from "react";
 import { useState } from "react";
-
+import { Link } from "react-router-dom";
+import {  SignedIn, SignedOut } from "@clerk/clerk-react";
 export const Home = () => {
     const [images, setImages] = useState(galleryList);
     return (
@@ -21,9 +22,20 @@ export const Home = () => {
     Welcome to Gallerie, a place to share your photos with
     the world.
 </p>
+
+<SignedOut>
 <p>
     Sign in to use the drag and drop feature to rearrange your photos.
 </p>
+</SignedOut>
+
+<SignedIn>
+<p>
+    <Link to="/gallery">Go to your gallery</Link> where you can drag and drop your photos.
+</p>
+</SignedIn>
+
+
                  </div>
                     <div className="image-container">
                         {Children.toArray(
